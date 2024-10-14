280154b0befef79d035e0312eb64cd5e864c145f
ahmadilham_13 <ahmad13ilham13@gmail.com>
2024-10-14 20:56:40 +0700

    (+) attendance handling

--- app/Livewire/Presensi.php
+++ app/Livewire/Presensi.php
@@ -2,17 +2,68 @@
 
 namespace App\Livewire;
 
+use App\Models\Attendance;
 use App\Models\Schedule;
+use Carbon\Carbon;
 use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class Presensi extends Component
 {
+    public $latitude;
+    public $longitude;
+    public $insideRadius = false;
+
     public function render()
     {
         $schedule = Schedule::query()->where('user_id', Auth::user()->id)->first();
+        $attendance = Attendance::query()->where('user_id', Auth::user()->id)
+                            ->whereDate('created_at', date('Y-m-d'))->first();
+
         return view('livewire.presensi', [
-            'schedule' => $schedule
+            'schedule' => $schedule,
+            'insideRadius' => $this->insideRadius,
+            'attendance' => $attendance,
         ]);
+    }
+
+    public function store()
+    {
+        $this->validate([
+            'latitude' => 'required',
+            'longitude' => 'required',
+        ]);
+
+        $schedule = Schedule::query()->where('user_id', Auth::user()->id)->first();
+
+        if ($schedule) {
+            $attendance = Attendance::query()->where('user_id', Auth::user()->id)
+                            ->whereDate('created_at', date('Y-m-d'))->first();
+
+            if (!$attendance) {
+                $attendance = Attendance::create([
+                    'user_id' => Auth::user()->id,
+                    'schedule_latitude' => $schedule->office->latitude,
+                    'schedule_longitude' => $schedule->office->longitude,
+                    'schedule_start_time' => $schedule->shift->start_time,
+                    'schedule_end_time' => $schedule->shift->end_time,
+                    'latitude' => $this->latitude,
+                    'longitude' => $this->longitude,
+                    'start_time' => Carbon::now()->toTimeString(),
+                    'end_time' => Carbon::now()->toTimeString(),
+                ]);
+            } else {
+                $attendance->update([
+                    'latitude' => $this->latitude,
+                    'longitude' => $this->longitude,
+                    'end_time' => Carbon::now()->toTimeString(),
+                ]);
+            }
+
+            return redirect()->route('presensi', [
+                'schedule' => $schedule,
+                'insideRadius' => false,
+            ]);
+        }
     }
 }
--- resources/views/livewire/presensi.blade.php
+++ resources/views/livewire/presensi.blade.php
@@ -12,21 +12,23 @@
                     <div class="grid grid-cols-1 md:grid-cols-2 gap-2 mt-4">
                         <div class="bg-gray-100 p-4 rounded-lg">
                             <h4 class="text-l font-bold mb-2">Jam Masuk</h4>
-                            <p><strong>09.02</strong></p>
+                            <p><strong>{{ $attendance->start_time }}</strong></p>
                         </div>
                         <div class="bg-gray-100 p-4 rounded-lg">
                             <h4 class="text-l font-bold mb-2">Jam Pulang</h4>
-                            <p><strong>17.00</strong></p>
+                            <p><strong>{{ $attendance->end_time }}</strong></p>
                         </div>
                     </div>
                 </div>
                 <div>
                     <h2 class="text-2xl font-bold mb-2">Presensi</h2>
                     <div id="map" class="mb-4 rounded-lg border border-gray-300" wire:ignore></div>
-                    <button type="button" onclick="tagLocation()" class="px-4 py-2 bg-blue-500 text-white rounded">Tag Location</button>
-                    @if ($insideRadius)
-                        <button type="button" class="px-4 py-2 bg-green-500 text-white rounded">Submit Presensi</button>
-                    @endif
+                    <form class="row g-3" wire:submit="store" enctype="multipart/form-data">
+                        <button type="button" onclick="tagLocation()" class="px-4 py-2 bg-blue-500 text-white rounded">Tag Location</button>
+                        @if ($insideRadius)
+                            <button type="submit" class="px-4 py-2 bg-green-500 text-white rounded">Submit Presensi</button>
+                        @endif
+                    </form>
                 </div>
             </div>
         </div>
@@ -69,6 +71,8 @@
 
                     if (isWithinRadius(lat, lng, office, radius)) {
                         component.set('insideRadius', true);
+                        component.set('latitude', lat);
+                        component.set('longitude', lng);
                     } else {
                         alert('You are not within the office radius!');
                     }
