5f93e3565d295820a6675a2a417341197610da53
ahmadilham_13 <ahmad13ilham13@gmail.com>
2024-10-31 10:32:43 +0700

    Leave Migrate, Model, Resource

---
+++ app/Models/Leave.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Leave extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'user_id',
+        'start_date',
+        'end_date',
+        'reason',
+        'status',
+        'note',
+    ];
+
+    public function user() : BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
